9ef2bbdc7f4dd7aef7351da1d8e2a9dd91d4164f
iman sadafi tehrani <66745129+ImanST@users.noreply.github.com>
2025-05-27 18:36:22 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [b"Hello World from Python (WSGI)!\n"]
+async def app(scope, receive, send):
+    await send({
+        'type': 'http.response.start',
+        'status': 200,
+        'headers': [
+            [b'content-type', b'text/plain'],
+        ],
+    })
+    await send({
+        'type': 'http.response.body',
+        'body': b"Hello World from Python (ASGI)!\n",
+    })
